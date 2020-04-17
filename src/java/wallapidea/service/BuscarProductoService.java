b0decbdcef9c71f253e36fa3c1fba5703220bdbc
alejandro2200 <alejandropalomino22@gmail.com>
2020-04-17 13:00:25 +0200

    solucion error

--- src/java/wallapidea/service/BuscarProductoService.java
+++ src/java/wallapidea/service/BuscarProductoService.java
@@ -7,6 +7,7 @@ package wallapidea.service;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.ejb.Stateless;
